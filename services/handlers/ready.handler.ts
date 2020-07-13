4f3d46dc4e8630a2030d12fc19773a942c86039d
theADAMJR <adamjf123@gmail.com>
2020-07-13 11:32:31 +0100

    fix auto mod (probably)

--- services/handlers/ready.handler.ts
+++ services/handlers/ready.handler.ts
@@ -5,12 +5,14 @@ import Music from '../../modules/music/music';
 import { bot } from '../../bot';
 import CommandService from '../command.service';
 import config from '../../config.json';
+import AutoMod from '../../modules/auto-mod/auto-mod';
 
 export default class ReadyHandler implements EventHandler {
     started = false;
     on = 'ready';
     
     constructor(
+        private autoMod = Deps.get<AutoMod>(AutoMod),
         private commandService = Deps.get<CommandService>(CommandService),        
         private music = Deps.get<Music>(Music)) {}
 
@@ -20,6 +22,7 @@ export default class ReadyHandler implements EventHandler {
         if (this.started) return;
         this.started = true;
         
+        await this.autoMod.init();
         await this.commandService.init();
 
         this.music.initialize();
