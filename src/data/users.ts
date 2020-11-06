7b19b0493921bfb11c06517eea5cd8766dd408dd
theADAMJR <adamjf123@gmail.com>
2020-11-06 18:18:02 +0000

    disallow bot accounts

--- src/data/users.ts
+++ src/data/users.ts
@@ -1,9 +1,14 @@
 import { SavedUser, UserDocument } from './models/user';
 import DBWrapper from './db-wrapper';
 import SnowflakeEntity from './snowflake-entity';
+import { bot } from '../bot';
 
 export default class Users extends DBWrapper<SnowflakeEntity, UserDocument> {
-    protected async getOrCreate({ id }: SnowflakeEntity) {        
+    protected async getOrCreate({ id }: SnowflakeEntity) {
+        const user = bot.users.cache.get(id);
+        if (user?.bot)
+            throw new TypeError(`Bots don't have accounts`);
+        
         const savedUser = await SavedUser.findById(id);
         if (savedUser
             && savedUser.premiumExpiration
